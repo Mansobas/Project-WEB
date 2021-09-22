4b8817a466910a8876a4f17a275128f2c66076ad
Mansobas <joaocarlosmansur@gmail.com>
2021-09-22 16:21:33 -0300

    Trabalhando com sessões.

---
+++ Aula/src/main/java/Sessões1/ServletSessoes1.java
@@ -0,0 +1,59 @@
+package Sessões;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletSessoes
+ */
+@WebServlet("/ServletSessoes")
+public class ServletSessoes extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServletSessoes() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    
+    public static final String NOME = "nome";
+    public static final String SOBRENOME = "sobrenome";
+    public static final String CPF = "cpf";
+    public static final String EMAIL = "email";
+    
+    
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter resposta = response.getWriter();
+		
+		resposta.write("<html><body>");
+		
+		String Nome = request.getParameter(NOME);
+		String Sobrenome = request.getParameter(SOBRENOME);
+		String CPF = request.getParameter(CPF);
+		String Email = request.getParameter(EMAIL);
+		
+		
+		if (autentica(Nome,Sobrenome,Cpf, Email)) {
+			HttpSession sessao = request.getSession();
+			sessao.setAttribute(NOME, Nome);
+			sessao.setAttribute(SOBRENOME, Sobrenome);
+			sessao.setAttribute(CPF, Cpf);
+			sessao.setAttribute(EMAIL, Email);
+			
+			
+			resposta.write("Bem vindo " + Nome + Sobrenome + Cpf + Email);
+		} else {
+			resposta.write("Tudo certo!");
+		
+					
+}
---
+++ Aula/src/main/webapp/NewFile.jsp
@@ -0,0 +1,20 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Dados pessoais</title>
+</head>
+<body>
+escreva seus dados pessoais:
+<form action="ServletSessoes1">
+Nome: <input type="text" name="nome"> 
+Sobrenome: <input type="text" name="sobrenome">
+CPF: <input type="text" name="cpf">
+EMAIL: <input type="text" name="email">
+Telefone: <input type="text" name="cep">
+<input type"submit" value="Cadastrar">
+</form>   
+</body>
+</html>
