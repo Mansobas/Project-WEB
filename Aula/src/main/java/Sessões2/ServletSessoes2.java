be509dd98c66070746a4717d7e02edff0821dc3b
Mansobas <joaocarlosmansur@gmail.com>
2021-09-22 17:03:09 -0300

    teste

--- Aula/src/main/java/Sessões2/ServletSessoes2.java
+++ Aula/src/main/java/Sessões2/ServletSessoes2.java
@@ -1,6 +1,7 @@
-package Sessões2;
+package Sessões;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -8,34 +9,51 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /**
- * Servlet implementation class ServletSessoes2
+ * Servlet implementation class ServletSessoes
  */
 @WebServlet("/ServletSessoes2")
-public class ServletSessoes2 extends HttpServlet {
+public class ServletSessoes extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public ServletSessoes2() {
+    public ServletSessoes() {
         super();
         // TODO Auto-generated constructor stub
     }
-
+    
+    public static final String NOME = "nome";
+    public static final String SOBRENOME = "sobrenome";
+    public static final String CPF = "cpf";
+    public static final String EMAIL = "email";
+    
+    
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter resposta = response.getWriter();
+		
+		resposta.write("<html><body>");
+		
+		String Nome = request.getParameter(NOME);
+		String Sobrenome = request.getParameter(SOBRENOME);
+		String CPF = request.getParameter(CPF);
+		String Email = request.getParameter(EMAIL);
+		
+		
+		if (autentica(Nome,Sobrenome,Cpf, Email)) {
+			HttpSession sessao = request.getSession();
+			sessao.setAttribute(NOME, Nome);
+			sessao.setAttribute(SOBRENOME, Sobrenome);
+			sessao.setAttribute(CPF, Cpf);
+			sessao.setAttribute(EMAIL, Email);
+			
+			
+			resposta.write("Bem vindo " + Nome + Sobrenome + Cpf + Email);
+		} else {
+			resposta.write("Tudo certo!");
+		
+					
 }
